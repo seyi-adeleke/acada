0b3a5b5c5b0598f459d411ea5f9525fb3a397b39
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-04-23 23:35:01 +0100

    created video model

---
+++ server/models/videos.js
@@ -0,0 +1,30 @@
+var mongoose = require( 'mongoose' );
+var Schema = mongoose.Schema;
+
+var requiredStringValidator = [
+    function (val) {
+        var testVal = val.trim();
+        return (testVal.length > 0)
+    },
+    // Custom error text...
+    '{PATH} cannot be empty' ];
+
+var videoSchema = new Schema({
+    member:{type: String},
+    title: {
+        type: String,
+        required: true,
+        validate: requiredStringValidator },
+    link: {
+        type: String,
+        required: true,
+        validate: requiredStringValidator },
+    tags: {
+        type: Array,
+        required: true,
+        validate: requiredStringValidator },
+    createdOn: { type: Date,
+        default: Date.now }
+});
+
+module.exports = mongoose.model( 'Video', videoSchema );
