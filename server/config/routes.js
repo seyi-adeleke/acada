7d388f8e74eae482ab2c033a3b7363dfb62307d4
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-04-23 22:54:00 +0100

    setup basic routing

---
+++ server/config/routes.js
@@ -0,0 +1,30 @@
+//var noteCtrl = require('../controllers/note.controller');
+
+module.exports = function(app){
+
+    app.get('/', function (req, res) {
+        res.render('index.ejs');
+    });
+
+
+
+
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+
+    app.get('/profile', isLoggedIn, function(req, res) {
+        res.render('profile.ejs', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+};
+function isLoggedIn(req, res, next) {
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+        return next();
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
