786b729fbf2496fcd9227bd5298ea39ad03d4888
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-04-23 23:38:08 +0100

    updated routing

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,6 @@
-//var noteCtrl = require('../controllers/note.controller');
+var videoCtrl = require('../controllers/video.controller');
 
-module.exports = function(app){
+module.exports = function(app,passport){
 
     app.get('/', function (req, res) {
         res.render('index',{
@@ -8,8 +8,32 @@ module.exports = function(app){
         });
     });
 
+    app.get('/newvideo', isLoggedIn, function(req, res){
+        return videoCtrl.getVideo(req, res);
+    });
 
 
+
+    app.get('/login', function(req, res) {
+        // render the page and pass in any flash data if it exists
+        res.render('login', { message: req.flash('loginMessage'), user : req.user });
+    });
+
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+    app.get('/signup', function(req, res) {
+        res.render('signup',{ message: req.flash('signupMessage'), user : req.user  });
+    });
+    app.post('/signup', passport.authenticate('local-signup',{
+            successRedirect:'/Newnote',
+            failureRedirect:'/signup',
+            failureFlash: true
+        }
+    ));
 
     app.get('/logout', function(req, res) {
         req.logout();
@@ -17,10 +41,17 @@ module.exports = function(app){
     });
 
 
+  // user management
+
     app.get('/profile', isLoggedIn, function(req, res) {
         res.render('profile.ejs', {
             user : req.user // get the user out of session and pass to template
         });
+    });
+
+
+    app.get('/signup', function(req, res) {
+        res.render('signup',{ message: req.flash('signupMessage'), user : req.user  });
     });
 };
 function isLoggedIn(req, res, next) {
