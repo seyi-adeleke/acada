be19822590edc55011c4812049d950aab94f981f
Seyi Adeleke <seyiadeleke42@yahoo.com>
2017-04-24 12:10:17 +0100

    fixed video controller bug

--- server/controllers/video.controller.js
+++ server/controllers/video.controller.js
@@ -4,7 +4,7 @@ var Users = require('../models/users');
 exports.list = function(req, res) {
     Video.find().sort({ createdOn: 'desc'}).limit(12).exec(function(err, videos){
         Users.find({},function(err,users){
-            res.render('videos',{notes: videos, user : req.user, users:users})
+            res.render('videos',{videos: videos, user : req.user, users:users})
         })
     })
 
@@ -16,19 +16,18 @@ exports.getVideo = function (req, res) {
 exports.createVideo = function(req, res) {
     var entry = new Video({
         member: req.user.firstName,
-        project: req.body.project,
-        workYesterday: req.body.workYesterday,
-        workToday: req.body.workToday,
-        impediment: req.body.impediment
+        title: req.body.title,
+        link: req.body.link,
+        tags: req.body.tags
     });
     entry.save(function (err) {
         if (err) {
             var errMsg = 'Sorry, there was an error saving the video. ' + err;
             console.log(err);
-            res.render('newvideos', { user : req.user, firstName:req.body.firstName, message: req.flash(errMsg) });
+            res.render('newvideo', { user : req.user, firstName:req.body.firstName, message: req.flash(errMsg) });
         }
         else {
-            console.log('Video was not saved!');
+            console.log('Video was saved!');
             res.redirect('/videos');
         }
     });
